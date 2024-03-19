cd5651c19b8f79d0e093a792d9447a9267ac3f65
james-mbuvi <152844337+james-mbuvi@users.noreply.github.com>
2024-03-19 18:19:21 +0300

    Update NavBar.tsx

--- src/Home/NavBar.tsx
+++ src/Home/NavBar.tsx
@@ -5,16 +5,9 @@ const NavBar = () => {
   return (
     <nav className='bg-gray-800 px-4 py-3 flex justify-between ml-64'>
       <div className='flex items-center text-xl'>
-        
-        {/* <span className='text-white font-semibold'>
-          search Member...
-        </span> */}
       </div>
       <div className='flex items-center gap-x-5'>
         <div className='relative md:w-65'>
-          {/* <span className='relative md:absolute inset-y-0-left-0 flex-center pl-2'>
-            <button className='p-1 focus:outline-none text-white md:text-black'><FaSearch /></button>
-          </span> */}
           <input type="text" className='w-full px-4 py-1 pl-12 rounded shadow outline-none hidden md:block' placeholder='Search Member...'/>
         </div>
         <div className='text-white'><FaBell className='w-6 h-6'/></div>

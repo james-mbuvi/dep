c3003ac19867a7f155279b3d3440a180debb7400
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-03-20 15:19:32 +0300

    Update the Contact List Form

--- src/Home/Sidebar.tsx
+++ src/Home/Sidebar.tsx
@@ -1,34 +1,45 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { FaCalendar, FaCog, FaDollarSign, FaHome, FaPhone, FaTasks, FaUsers, FaWallet } from 'react-icons/fa'
 import { FaUserGroup } from 'react-icons/fa6'
+import Contact from './Contact'
 
 const Sidebar = () => {
   
+  const [showContact, setShowContact] = useState(false);
   
-    const handleHomeClick = () => {
+  const handleContact = () => {
+    setShowContact(!showContact);
+  };
+    const handleHomeClick= () => {
       // Add your logic here for what should happen when the Home button is clicked
       console.log('Home button clicked');
     }
 
+
   return (
+    
     <div className='w-64 bg-blue-800 fixed px-4 py-2'>
       <div className='my-2 mb-4'>
-        <h1 className='text-2x text-white font-bold'>Admin Dashboard</h1>
+      <h1 className='text-2x text-white font-bold'>Admin Dashboard</h1>
       </div>
       <hr />
       <ul className='mt-3 text-white font-bold'>
         <li className='mb-2 rounded hover:shadow hover:bg-gray-800 py-2'>
         <button onClick={handleHomeClick} className='px-3'>
-            <FaHome className='inline-block w-6 h-6 mr-2 -mt-2'></FaHome>
-            Home
-          </button>
+        <FaHome className='inline-block w-6 h-6 mr-2 -mt-2'></FaHome>
+        Home
+        </button>
         </li>
         <li className='mb-2 rounded hover:shadow hover:bg-gray-800 py-2'>
-        <button onClick={handleHomeClick} className='px-3'>
-            <FaPhone className='inline-block w-6 h-6 mr-2 -mt-2'></FaPhone>
-            Contacts 
-          </button>
+        <button onClick={handleContact} className='px-3'>
+         <span className='text-black'>
+        <FaPhone className='inline-block w-6 h-6 mr-2 -mt-1'></FaPhone>
+         </span>
+         Contact
+        </button>
+        {showContact && <Contact />}
         </li>
+
         <li className='mb-2 rounded hover:shadow hover:bg-gray-800 py-2'>
         <button onClick={handleHomeClick} className='px-3'>
             <FaTasks className='inline-block w-6 h-6 mr-2 -mt-2'></FaTasks>
